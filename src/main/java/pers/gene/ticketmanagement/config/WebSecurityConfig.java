86322b9620c8728c786b87ad544256db6b349a37
Gene1994 <geneq@hotmail.com>
2018-05-27 21:28:33 +0800

    Spring security 验证和授权分别检查

--- src/main/java/pers/gene/ticketmanagement/config/WebSecurityConfig.java
+++ src/main/java/pers/gene/ticketmanagement/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import pers.gene.ticketmanagement.service.CustomAuthenticationProvider;
+import pers.gene.ticketmanagement.service.CustomerService;
 import pers.gene.ticketmanagement.web.filter.JWTAuthenticationFilter;
 import pers.gene.ticketmanagement.web.filter.JWTLoginFilter;
 
@@ -19,7 +20,7 @@ import pers.gene.ticketmanagement.web.filter.JWTLoginFilter;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService;
-
+    private CustomerService customerService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public WebSecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
@@ -27,21 +28,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    //HTTP请求安全处理
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .anyRequest().authenticated()
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+
                 .antMatchers("/index","/customer/register").permitAll()
+
+                //以 "/admin/" 开头的URL只能让拥有 "ROLE_ADMIN"角色的用户访问。
+                .antMatchers("/admin/**").hasRole("ADMIN")
+
+                //任何以"/db/" 开头的URL需要同时具有 "ROLE_ADMIN" 和 "ROLE_DBA"权限的用户才可以访问。
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+
+                //任何以"/db/" 开头的URL只需要拥有 "ROLE_ADMIN" 和 "ROLE_DBA"其中一个权限的用户才可以访问。
+                .antMatchers("/db/**").hasAnyRole("ADMIN", "DBA")
+
+                //尚未匹配的任何URL都要求用户进行身份验证
+                .anyRequest().authenticated()
+
                 .and()
                 .addFilter(new JWTLoginFilter(authenticationManager()))
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+
                 .formLogin()
-                .loginPage("/customer/login").permitAll()
+                .loginPage("/customer/login")
                 .loginProcessingUrl("/customer/login")
-                .failureUrl("/customer/fail")
-                .defaultSuccessUrl("/customer/index");
-//                .and()
-//                .rememberMe().tokenValiditySeconds(1209600).key("mykey")
+                .failureUrl("/customer/fail").permitAll()
+                .defaultSuccessUrl("/customer/index")
+
+                .and()
+                //开启cookie储存用户信息，并设置有效期为14天，指定cookie中的密钥
+                .rememberMe().tokenValiditySeconds(1209600).key("mykey");
+
 //                .and()
 //                .logout()
                 //指定登出的url
@@ -51,14 +74,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .permitAll();
     }
 
+    //身份验证管理生成器
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         //并根据传入的AuthenticationManagerBuilder中的userDetailsService方法来接收我们自定义的认证方法。
         //且该方法必须要实现UserDetailsService这个接口。
-        auth.authenticationProvider(new CustomAuthenticationProvider(userDetailsService,bCryptPasswordEncoder));
+        auth.userDetailsService(new CustomerService())
+                //密码使用BCryptPasswordEncoder()方法验证，因为这里使用了BCryptPasswordEncoder()方法验证。所以在注册用户的时候在接收前台明文密码之后也需要使用BCryptPasswordEncoder().encode(明文密码)方法加密密码。
+                .passwordEncoder(new BCryptPasswordEncoder());;
+
 
     }
 
+    //WEB安全
     @Override
     public void configure(WebSecurity web) throws Exception {
         //解决静态资源被拦截的问题
--- src/main/java/pers/gene/ticketmanagement/service/CustomerService.java
+++ src/main/java/pers/gene/ticketmanagement/service/CustomerService.java
@@ -168,4 +168,5 @@ public class CustomerService implements UserDetailsService {
         }
         return new org.springframework.security.core.userdetails.User(customer.getUserName(), customer.getPassword(), emptyList());
     }
+
 }
