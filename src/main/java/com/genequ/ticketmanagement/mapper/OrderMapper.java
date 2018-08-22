044ce0ca9adc080cdeb2b6dda500a0e9b35ddc25
Gene <geneq@hotmail.com>
2018-08-22 18:46:12 +0800

    [mod]1.添加configuration maven依赖

--- src/main/java/com/genequ/ticketmanagement/mapper/OrderMapper.java
+++ src/main/java/com/genequ/ticketmanagement/mapper/OrderMapper.java
@@ -12,21 +12,21 @@ import java.util.List;
 public interface OrderMapper {
     String ORDER_COLUMN_LIST = " id, order_no, user_id, shipping_id, payment, payment_type, postage, status, payment_time, send_time, end_time, close_time, create_time, update_time ";
 
-    int deleteByPrimaryKey(Integer id);
+//    int deleteByPrimaryKey(Integer id);
 
     @Insert("insert into order (id, order_no, user_id, shipping_id, payment, payment_type, postage, status, payment_time, send_time, end_time, close_time, create_time, update_time) " +
             "values (#{id,jdbcType=INTEGER}, #{orderNo,jdbcType=BIGINT}, #{userId,jdbcType=INTEGER}, #{shippingId,jdbcType=INTEGER}, #{payment,jdbcType=DECIMAL}, #{paymentType,jdbcType=INTEGER}, " +
             " #{postage,jdbcType=INTEGER}, #{status,jdbcType=INTEGER}, #{paymentTime,jdbcType=TIMESTAMP},  #{sendTime,jdbcType=TIMESTAMP}, #{endTime,jdbcType=TIMESTAMP}, #{closeTime,jdbcType=TIMESTAMP}, now(), now())")
     int insert(Order record);
 
-    int insertSelective(Order record);
+//    int insertSelective(Order record);
 
-    Order selectByPrimaryKey(Integer id);
+//    Order selectByPrimaryKey(Integer id);
 
     @UpdateProvider(type = TutorDynaSqlProvider.class, method = "updateByPrimaryKeySelectiveSQL")
     int updateByPrimaryKeySelective(Order order);
 
-    int updateByPrimaryKey(Order record);
+//    int updateByPrimaryKey(Order record);
 
 
     @Select("SELECT" + ORDER_COLUMN_LIST + "from mmall_order where order_no = #{orderNo} and user_id = #{userId}")
